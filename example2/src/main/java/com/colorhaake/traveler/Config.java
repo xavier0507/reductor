c076c3ddf4eaff546cbfdbf30158edac8b5d2af4
colorhaake <colorhaake@gmail.com>
2017-10-27 14:09:58 +0800

    upgrade to android studio 3.0

--- example2/src/main/java/com/colorhaake/traveler/Config.java
+++ example2/src/main/java/com/colorhaake/traveler/Config.java
@@ -3,5 +3,5 @@ package com.colorhaake.traveler;
  * Created by josephcheng on 2017/3/25.
  */
 public class Config {
-    public static final String BASE_API_URL = "https://api.klook.com/";
+    public static final String BASE_API_URL = "";
 }
