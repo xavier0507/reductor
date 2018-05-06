5bb74ee2dc922249e798c347345aafed63810e95
colorhaake <colorhaake@gmail.com>
2018-05-06 14:54:00 +0800

    refactor OnCompoundDrawableTouchListener

--- example3/src/main/java/javatest/colorhaake/com/example3/view/utils/OnCompoundDrawableTouchListener.kt
+++ example3/src/main/java/javatest/colorhaake/com/example3/view/utils/OnCompoundDrawableTouchListener.kt
@@ -11,36 +11,36 @@ interface OnCompoundDrawableTouchListener : View.OnTouchListener {
     override fun onTouch(v: View?, event: MotionEvent?): Boolean {
         if (v == null || event == null || v !is TextView) return false
 
-        val textView: TextView = v
         val x = event.x.toInt()
         val y = event.y.toInt()
+
+        val textView: TextView = v
         val drawables = textView.compoundDrawables
 
         val onTouchIndexedDrawable = drawables.withIndex()
                 .filter { it.value != null }
                 .map { IndexedValue(it.index, getDrawableBounds(it.index, it.value, textView)) }
                 .firstOrNull { it.value.contains(x, y) }
+                ?: return false
 
-        if (onTouchIndexedDrawable != null) {
-            with (onTouchIndexedDrawable) {
-                val relativeEvent = MotionEvent.obtain(
-                        event.downTime,
-                        event.eventTime,
-                        event.action,
-                        event.x - value.left,
-                        event.y - value.top,
-                        event.metaState
-                )
-                return onDrawableTouch(v, index, value, relativeEvent)
-            }
+        with (onTouchIndexedDrawable) {
+            val relativeEvent = MotionEvent.obtain(
+                    event.downTime,
+                    event.eventTime,
+                    event.action,
+                    event.x - value.left,
+                    event.y - value.top,
+                    event.metaState
+            )
+            return onDrawableTouch(v, index, value, relativeEvent)
         }
-
-        return false
     }
 
     private fun getDrawableBounds(index: Int, drawable: Drawable, view: TextView): Rect {
-        val drawableLeftWidth = view.compoundDrawables[LEFT]?.takeIf { it.isVisible }?.intrinsicWidth ?: 0
-        val drawableRightWidth = view.compoundDrawables[RIGHT]?.takeIf { it.isVisible }?.intrinsicWidth ?: 0
+        val drawableLeftWidth = view.compoundDrawables[LEFT]
+                ?.takeIf(Drawable::isVisible)?.intrinsicWidth ?: 0
+        val drawableRightWidth = view.compoundDrawables[RIGHT]
+                ?.takeIf(Drawable::isVisible)?.intrinsicWidth ?: 0
         val horizontalDrawableOffset = drawableLeftWidth - drawableRightWidth
         return when (index) {
             LEFT -> Rect(
